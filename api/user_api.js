73d5e2047595319f7d82a5f9914e29dcbdf03f00
NoNameMusic <130332761+Music22761@users.noreply.github.com>
2024-03-09 13:15:22 +0700

    latest

--- api/user_api.js
+++ api/user_api.js
@@ -27,86 +27,25 @@ router.get("/id", (req, res) => {
   //   res.send("this is Admin Page")
 });
 
-router.get("/code/:id", (req, res) => {
-  const search = `"%${req.params.id}%"`
+
+
+router.get("/code/:search", (req, res) => {
+  const search = `%${req.params.search}%`
   console.log(search)
   conn.query(
-    // "select * from User_En where code = " + req.query.code,
     "select * from User_En where code like ? or fname like ? or lname like ? or nickname like ?",
+    // "select * from User_En where code like ?",
     [search, search, search, search],
-    (err, result, fields) => {
+    (err, result) => {
       if(err) {
-        res.json(result);
-      }else {
         res.json(err)
+      }else {
+        console.log("OK");
+        res.json(result);
       }
     }
   );
-  // if (err) {
-    
-  // } else {
-  //   res.send("call get in Users with Query Param " + req.query.code);
-  // }
-  //   res.send("this is Admin Page")
 });
-
-// router.get("/fname/:fname", (req, res) => {
-//   if (req.query.id) {
-//     conn.query(
-//       "select * from User_En where fname = " + req.query.fname,
-//       (err, result, fields) => {
-//         res.json(result);
-//       }
-//     );
-//   } else {
-//     res.send("call get in Users with Query Param " + req.query.fname);
-//   }
-//   //   res.send("this is Admin Page")
-// });
-
-// router.get("/lname/:lname", (req, res) => {
-//   if (req.query.id) {
-//     conn.query(
-//       "select * from User_En where lname = " + req.query.lname,
-//       (err, result, fields) => {
-//         res.json(result);
-//       }
-//     );
-//   } else {
-//     res.send("call get in Users with Query Param " + req.query.lname);
-//   }
-//   //   res.send("this is Admin Page")
-// });
-// router.post('/', (req, res) => {
-//   console.log(req.body);
-
-//   let uesrData = req.body;
-
-//   // สร้างคำสั่ง SQL เพื่อเพิ่มข้อมูลผู้ใช้
-//   let sql = "INSERT INTO `User` (`email`, `password`, `name_th`, `name_en`, `account_name`, `profile_picture`, `qr_code`, `role`) VALUES (?,?,?,?,?,?,?,?)";
-//   sql = mysql.format(sql, [
-//     uesrData.email,
-//     uesrData.password,
-//     uesrData.name_th,
-//     uesrData.name_en,
-//     uesrData.account_name,
-//     uesrData.profile_picture,
-//     uesrData.qr_code,
-//     uesrData.role,
-//   ]);
-
-//   // Execute the SQL statement
-//   conn.query(sql, (err, result) => {
-//     if (err) {
-//       console.error('Error inserting data: ', err);
-//       res.status(500).send('Error inserting data' + uesrData);
-//       return;
-//     }
-
-//     console.log('Data inserted successfully');
-//     res.status(200).send('Data inserted successfully');
-//   });
-// });
 
 router.post('/', (req, res) => {
   console.log(req.body);
